02eee58ce20f6fc795bb2f580aac1db3cbef84b6
Usman Awan <uawan8154@gmail.com>
2024-05-15 12:55:08 +0100

    Updated Work.jsx Component, made work portfolio more responsive.

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,7 +20,7 @@ const Work = () => {
                         <a href="https://e-commerce-store-web-app.vercel.app/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                         <a href="https://github.com/UsmanDevCraft/e-commerce" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard">
                         <img src="/ecommerce_img_001.png" className="card-img-top" alt="/ecommerce_img_001.png" />
                     </div>
                 </div>
@@ -32,13 +32,13 @@ const Work = () => {
             {/* ------------------ PROJECT#01 ------------------ */}
             <div className="card my-5 bg_card_color">
                 <div className='d-flex flex-column flex-md-row justify-content-center align-items-center'>
-                    <div className="card-body col-md-4">
+                    <div className="card-body col-md-4 order-md-2">
                         <h5 className="card-title ProjectsTag">Cloudbook</h5>
                         <p className="card-text">This is an online notes saving web app which is used to store the data of a specific user only. Using the Login and Signup functionality, one can easily create an account and login to create, read, update, and delete their notes. This is made from React + Vite, MongoDB, Express, and Node, and Bootstrap.</p>
                         <a href="https://cloud-book-mern-web-app.vercel.app/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                         <a href="https://github.com/UsmanDevCraft/Cloudbook" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard order-md-1">
                         <img src="/cloudbook_ss_3.png" className="card-img-top" alt="/cloudbook_ss_3.png" />
                     </div>
                 </div>
@@ -55,7 +55,7 @@ const Work = () => {
                         <a href="https://usmandevcraft.github.io/Modern-Website-Landing-Page/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/Modern-Website-Landing-Page" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard">
                         <img src="/Modern_Web_Landing_Page_001.png" className="card-img-top" alt="/Modern_Web_Landing_Page_001.png" />
                     </div>
                 </div>
@@ -65,13 +65,13 @@ const Work = () => {
             {/* ------------------ PROJECT#03 ------------------ */}
             <div className="card my-5 bg_card_color">
                 <div className='d-flex flex-column flex-md-row justify-content-center align-items-center'>
-                    <div className="card-body col-md-4">
+                    <div className="card-body col-md-4 order-md-2">
                         <h5 className="card-title ProjectsTag">textEditor</h5>
                         <p className="card-text">This is a text editor which can be used for text editing. It is made from react, bootstrap and vite.</p>
                         <a href="https://text-editor-gold-nine.vercel.app/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/textEditor" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard order-md-1">
                         <img src="/TextEditor_001.png" className="card-img-top" alt="/TextEditor_001.png" />
                     </div>
                 </div>
@@ -87,7 +87,7 @@ const Work = () => {
                         <a href="https://news-hub-news-delivering-website.vercel.app/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/NewsHub" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard">
                         <img src="/NewsHub_ss_01.png" className="card-img-top" alt="/NewsHub_ss_01.png" />
                     </div>
                 </div>
@@ -97,13 +97,13 @@ const Work = () => {
             {/* ------------------ PROJECT#04 ------------------ */}
             <div className="card my-5 bg_card_color">
                 <div className='d-flex flex-column flex-md-row justify-content-center align-items-center'>
-                    <div className="card-body col-md-4">
+                    <div className="card-body col-md-4 order-md-2">
                         <h5 className="card-title ProjectsTag">SNAKE-GAME</h5>
                         <p className="card-text">This is a responsive snake game provided with different speed options to play with. It is coded with pure HTML, CSS and vanilla JS.</p>
                         <a href="https://usmandevcraft.github.io/SNAKE-GAME/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/SNAKE-GAME" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard order-md-1">
                         <img src="/Snake_Game_001.png" className="card-img-top" alt="/Snake_Game_001.png" />
                     </div>
                 </div>
@@ -119,7 +119,7 @@ const Work = () => {
                         <a href="https://usmandevcraft.github.io/Responsive-SPOTIFY-Clone/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/Responsive-SPOTIFY-Clone" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard">
                         <img src="/spotify_clone_ss.png" className="card-img-top" alt="/spotify_clone_ss.png" />
                     </div>
                 </div>
@@ -129,13 +129,13 @@ const Work = () => {
             {/* ------------------ PROJECT#06 ------------------ */}
             <div className="card my-5 bg_card_color">
                 <div className='d-flex flex-column flex-md-row justify-content-center align-items-center'>
-                    <div className="card-body col-md-4">
+                    <div className="card-body col-md-4 order-md-2">
                         <h5 className="card-title ProjectsTag">Responsive-BUBBLE-GAME</h5>
                         <p className="card-text">This is a responsive bubble game in which you hit bubbles with same number as that of hit targtet given, each correct hit gives a score of 10 and after each hit, all numbers on screen changes with change in target. It is made with HTML, CSS and JS only.</p>
                         <a href="https://usmandevcraft.github.io/Responsive-BUBBLE-GAME/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/Responsive-BUBBLE-GAME" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard order-md-1">
                         <img src="/bubble_game_ss.png" className="card-img-top" alt="/bubble_game_ss.png" />
                     </div>
                 </div>
@@ -151,7 +151,7 @@ const Work = () => {
                         <a href="https://docs-app-sigma.vercel.app/" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/DocsApp" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard">
                         <img src="/docs_app_001.png" className="card-img-top" alt="/docs_app_001.png" />
                     </div>
                 </div>
@@ -161,13 +161,13 @@ const Work = () => {
             {/* ------------------ PROJECT#08 ------------------ */}
             <div className="card bg_card_color">
                 <div className='d-flex flex-column flex-md-row justify-content-center align-items-center'>
-                    <div className="card-body col-md-4">
+                    <div className="card-body col-md-4 order-md-2">
                         <h5 className="card-title ProjectsTag">python-backend-main</h5>
                         <p className="card-text">A Machine Learning project with react front end for overlapped speech seperation.</p>
                         <a href="python-backend-main.vercel.app" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark d-flex justify-content-center align-items-center gap-2"><FaEye />Live</button></a>
                        <a href="https://github.com/UsmanDevCraft/python-backend-main" className='anchor_live' target="target_main"><button type="button" className="btn btn-dark mt-1 d-flex justify-content-center align-items-center gap-2"><FaGithub />Source</button></a>
                     </div>
-                    <div className="card col-md-8">
+                    <div className="card col-md-8 imgCard order-md-1">
                         <img src="/FYP_Frontend_pic.png" className="card-img-top" alt="/FYP_Frontend_pic.png" />
                     </div>
                 </div>
